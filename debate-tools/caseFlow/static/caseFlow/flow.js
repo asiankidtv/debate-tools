847eca5f5f482804cff426b8f3a1a2da74462083
Ethan <90428291+asiankidtv@users.noreply.github.com>
2026-02-21 22:39:29 -0500

    0.0.28 modal overhaul

--- debate-tools/caseFlow/static/caseFlow/flow.js
+++ debate-tools/caseFlow/static/caseFlow/flow.js
@@ -54,6 +54,58 @@ let flowId = -1;
 let deleteId = -1;
 // const flows = flow data taken from jinja
 
+/**
+ * Modal Control
+ * Works by assigning or removing the 'is-active' class to modals.
+ * For every button with the 'js-modal-trigger' class, it connects it to the modal that is in its data-target value.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+        closeModal($modal);
+        });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+        openModal($target);
+        });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+        closeModal($target);
+        });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        if(event.key === "Escape") {
+        closeAllModals();
+        }
+    });
+
+    // Adds an even to 'no' buttons:
+    document.getElementById('no-delete-button').addEventListener("click", function(e) {
+        closeAllModals();
+    });
+});
 
 /**
  * Save Case Button
